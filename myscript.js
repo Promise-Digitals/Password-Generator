71bbedcde7790817415dcab42936c1b43c491568
Promise-Digitals <promiseedeh025@gmail.com>
2024-07-21 22:01:48 +0100

    updated the design

--- myscript.js
+++ myscript.js
@@ -10,10 +10,10 @@ const allChar = upperCase + lowerCase + numbers + symbols;
 
 function createPassword() {
     let password = "";
-    password += upperCase[Math.floor(Math.random() * upperCase.length)];
-    password += lowerCase[Math.floor(Math.random() * lowerCase.length)];
-    password += numbers[Math.floor(Math.random() * numbers.length)];
-    password += symbols[Math.floor(Math.random() * symbols.length)];
+    // password += upperCase[Math.floor(Math.random() * upperCase.length)];
+    // password += lowerCase[Math.floor(Math.random() * lowerCase.length)];
+    // password += numbers[Math.floor(Math.random() * numbers.length)];
+    // password += symbols[Math.floor(Math.random() * symbols.length)];
 
     while (length > password.length) {
         password += allChar[Math.floor(Math.random() * allChar.length)];
